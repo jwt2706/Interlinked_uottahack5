c24b994b0c431876b64a3eecc75b0ca2e37b10bd
jwt2706 <jwt2home@gmail.com>
2023-02-11 10:50:59 -0500

    working extension format

--- popup/popup.js
+++ popup/popup.js
@@ -1,2 +1,9 @@
 let numOfComments = 0;
-document.getElementById("num").innerHTML = "There are "+numOfComments+" comments on this page.";
+let numHTML = document.getElementById("num").innerHTML;
+numHTML = "There are "+numOfComments+" comments on this page.";
+
+/*numOfComments++;
+numHTML = "There are "+numOfComments+" comments on this page.";
+let element = document.getElementsByClassName("GridItemStats--1wyj6t9 fYzdSX");
+let newElement = '<div id="two">two</div>'
+element.insertAdjacentHTML( 'afterend', newElement )*/

87de83a84da89e03c2c2ca6906b75a8160259c0c
jwt2706 <jwt2home@gmail.com>
2023-02-11 17:00:23 -0500

    quickfix

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,7 @@
 let numOfComments = 0;
 document.getElementById("num").innerHTML = "There are "+numOfComments+" comments on this page.";
 
-for (let i=0;i<5;numOfComments++) {
+for (let i=0;i<numOfComments;i++) {
     window["currentMsg"]=i;
     currentMsg = document.createElement("p");
     currentMsg.innerHTML = "<strong>"+i+". This is a paragraph.";

7eba7c376ad2355d6c01006bb615dde0c954847d
jwt2706 <jwt2home@gmail.com>
2023-02-11 16:56:18 -0500

    IT WORKS

--- popup/popup.js
+++ popup/popup.js
@@ -1,2 +1,13 @@
 let numOfComments = 0;
 document.getElementById("num").innerHTML = "There are "+numOfComments+" comments on this page.";
+
+//const para = document.createElement("p");
+//para.innerHTML = "This is a paragraph.";
+//document.getElementById("comments").appendChild(para);
+
+for (let i=0;i<5;i++) {
+    window["currentMsg"]=i;
+    currentMsg = document.createElement("p");
+    currentMsg.innerHTML = "<strong>"+i+". This is a paragraph.";
+    document.getElementById("comments").appendChild(currentMsg);
+}

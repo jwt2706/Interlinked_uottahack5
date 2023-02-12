37194180ef2feb442357b5b661c4acaa36532bf5
jwt2706 <jwt2home@gmail.com>
2023-02-11 21:30:22 -0500

    hearts

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,20 @@
 let numOfComments = 0;
 document.getElementById("num").innerHTML = "There are "+numOfComments+" comments on this page.";
 
-for (let i=0;i<numOfComments;i++) {
+for (let i=0;i<7;i++) {
     window["currentMsg"]=i;
+    window["currentMsgCheckbox"]=i+'checkbox';
+    window["currentMsgLabel"]=i+'label';
     currentMsg = document.createElement("p");
-    currentMsg.innerHTML = "<strong>"+i+". This is a paragraph.";
+    currentMsgCheckbox = document.createElement("input");
+    currentMsgCheckbox.type = "checkbox";
+    currentMsgLabel = document.createElement("label");
+    currentMsg.innerHTML = i+1+'. "This is a Comment."';
+    currentMsgCheckbox.id = "heart"+i;
+    currentMsgLabel.htmlFor = "heart"+i;
+    currentMsgLabel.innerHTML = '&#9829';
     document.getElementById("comments").appendChild(currentMsg);
+    document.getElementById("comments").appendChild(currentMsgCheckbox);
+    document.getElementById("comments").appendChild(currentMsgLabel);
 }
+
